c6f753c6226eea8db93b6285b1514b53c0376af2
Alan Keown <bcycadata@outlook.com>
2024-07-29 17:21:04 +1000

    AppBars set

---
+++ src/components/page/navigation/Navbar.svelte
@@ -0,0 +1,65 @@
+<script>
+	import { AppBar } from '@skeletonlabs/skeleton-svelte';
+	import { page } from '$app/stores';
+	import Logo from '$components/page/navigation/Logo.svelte';
+</script>
+
+<AppBar background="bg-primary-400 p-px" {lead} {trail}>
+	<div class="flex h-full flex-auto items-center justify-center">
+		<a
+			class:active={$page.url.pathname.endsWith('/')}
+			href="/"
+			class="btn-base btn mr-2 rounded-xl bg-tertiary-400 py-2 font-semibold">Home</a
+		>
+		<a
+			class:active={$page.url.pathname.endsWith('/about')}
+			href="/about"
+			class="btn-base btn mr-2 rounded-xl bg-tertiary-400 py-2 font-semibold">About</a
+		>
+		<a
+			class:active={$page.url.pathname.endsWith('/contact')}
+			href="/contact"
+			class="btn-base sm: btn rounded-xl bg-tertiary-400 py-2 font-semibold">Contact Us</a
+		>
+	</div>
+</AppBar>
+
+{#snippet lead()}
+	<Logo />
+{/snippet}
+
+{#snippet trail()}
+	<div class="flex h-full items-center">
+		{#if $page?.data?.user}
+			{#if $page?.data?.role?.split('_')[0] === 'admin'}
+				<a
+					class:active={$page.url.pathname.includes('/admin')}
+					href="/admin"
+					class="btn-base btn mr-2 rounded-xl bg-primary-500 font-semibold text-surface-50"
+					>Administrator</a
+				>
+			{/if}
+			<a
+				class:active={$page.url.pathname.includes('/profile')}
+				href="/profile"
+				class="btn-base btn mr-2 rounded-xl bg-primary-500 font-semibold text-surface-50">Profile</a
+			>
+			<form method="POST" action="/api/auth/signout">
+				<button
+					class="btn-base btn mr-2 rounded-xl bg-primary-500 font-semibold text-surface-50"
+					type="submit"
+				>
+					Sign Out
+				</button>
+			</form>
+		{:else}
+			<a
+				class:active={$page.url.pathname.endsWith('/signin')}
+				href="/auth/signin"
+				class="btn-base btn mr-2 rounded-xl bg-primary-500 font-semibold text-surface-50"
+			>
+				Sign In
+			</a>
+		{/if}
+	</div>
+{/snippet}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,66 @@
 <script>
-	import '../app.css';
+	import Footer from '$components/page/Footer.svelte';
+	import Navbar from '$components/page/navigation/Navbar.svelte';
+	import '../app.postcss';
+	import { onMount } from 'svelte';
+
+	let isDarkMode = false;
+
+	onMount(() => {
+		// Check for user's preferred color scheme
+		if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			isDarkMode = true;
+			document.documentElement.classList.add('dark');
+		}
+
+		// Listen for changes in color scheme preference
+		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+			isDarkMode = e.matches;
+			if (isDarkMode) {
+				document.documentElement.classList.add('dark');
+			} else {
+				document.documentElement.classList.remove('dark');
+			}
+		});
+	});
 </script>
 
-<slot></slot>
+<div class="app-container">
+	<Navbar />
+
+	<main>
+		<slot />
+	</main>
+
+	<Footer />
+</div>
+
+<style lang="postcss">
+	:global(html, body) {
+		@apply m-0 h-full p-0;
+	}
+
+	.app-container {
+		@apply flex min-h-full flex-col;
+	}
+
+	.fixed-header {
+		@apply fixed left-0 right-0 top-0 z-10 bg-surface-100 dark:bg-surface-800;
+	}
+
+	.fixed-header nav {
+		@apply flex items-center justify-between;
+	}
+
+	.fixed-header ul {
+		@apply m-0 flex list-none gap-4 p-0;
+	}
+
+	main {
+		@apply mb-16 mt-16 flex-grow p-4; /* Adjust mt-16 and mb-16 based on your header/footer height */
+	}
+
+	.fixed-footer {
+		@apply fixed bottom-0 left-0 right-0 bg-primary-600 p-4 text-center dark:bg-surface-800;
+	}
+</style>
